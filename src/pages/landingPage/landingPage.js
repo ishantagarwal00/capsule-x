1256a4a80d2f14778ae05204e7806827828cd028
Ishant Agarwal <ishantagarwal32@gmail.com>
2023-07-23 19:30:27 +0530

    feat(landing-page): banner add

---
+++ src/pages/landingPage/landingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "../../components/banner";
+
+const LandingPage = () => {
+  return (
+    <>
+      <Banner />
+    </>
+  );
+};
+
+export default LandingPage;
